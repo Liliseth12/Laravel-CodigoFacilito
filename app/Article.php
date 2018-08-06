6e6a3512ff1553a968805a91837f924efdaea31c
Liseth Gutierrez <liliseth12@gmail.com>
2018-08-05 21:41:20 -0400

    Curso Laravel hasta relaciones. Cap. 7

---
+++ app/Article.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    //
+    protected $table = "articles";
+
+    protected $fillable = ['title', 'content', 'category_id', 'user_id' ];
+
+    public function category(){
+    	return $this->belongsTo('App\Category');
+    }
+
+    public function user(){
+    	return $this->belongsTo('App\User');
+    }
+
+    public function images(){
+    	return $this->hasMany('App\Image');
+    }
+
+    public function tags(){
+    	return $this->belongsToMany('App\Tag');
+    }
+}
